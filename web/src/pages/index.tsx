743bd26d8be3f040cac9e0936297c299434de132
iwatani-kenji <kleversonkenji@gmail.com>
2022-07-30 13:04:38 -0300

    finishing basic authentication flow

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,13 +1,29 @@
-import { useUser } from "@auth0/nextjs-auth0";
+import { getSession } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
 
 export default function Home() {
-  const { user } = useUser();
-
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <a href="/api/auth/login">Login</a>
-    </div>
-  );
+  return null;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+
+  // se não estiver autenticado
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  }
+  // se estiver autenticado
+  else {
+    return {
+      redirect: {
+        destination: "/app",
+        permanent: false,
+      },
+    };
+  }
+};
