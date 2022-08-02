22e53f01eac57e34aa4db46ae7fafec8a66945ce
iwatani-kenji <kleversonkenji@gmail.com>
2022-08-01 23:25:45 -0300

    implementing students listing functionality

--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -1,5 +1,16 @@
+import { AuthorizationGuard } from './../../../../../purchases/src/http/auth/authorization.guard';
 import { Student } from './../models/student';
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { StudentsService } from 'src/services/students.service';
 
 @Resolver(() => Student)
-export class StudentsResolver {}
+export class StudentsResolver {
+  constructor(private studentsService: StudentsService) {}
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentsService.listAllStudents();
+  }
+}
