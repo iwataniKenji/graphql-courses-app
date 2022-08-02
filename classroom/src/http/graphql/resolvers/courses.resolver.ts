95234fdd8a88bda7f362ab3799ca8494fc4c6daf
iwatani-kenji <kleversonkenji@gmail.com>
2022-08-01 23:28:10 -0300

    implementing courses listing functionality

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -1,5 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { CoursesService } from './../../../services/courses.service';
+import { Query, Resolver } from '@nestjs/graphql';
 import { Course } from '../models/course';
+import { UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from '../../../../../purchases/src/http/auth/authorization.guard';
 
 @Resolver(() => Course)
-export class CoursesResolver {}
+export class CoursesResolver {
+  constructor(private coursesService: CoursesService) {}
+
+  @Query(() => [Course])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.coursesService.listAllCourses();
+  }
+}
