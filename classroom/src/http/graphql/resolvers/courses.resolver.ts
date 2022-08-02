ce7d099c7d88f091a6f98d8292c368867d95923b
iwatani-kenji <kleversonkenji@gmail.com>
2022-08-01 23:31:30 -0300

    implementing enrollment listing functionality

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -10,7 +10,7 @@ export class CoursesResolver {
 
   @Query(() => [Course])
   @UseGuards(AuthorizationGuard)
-  students() {
+  courses() {
     return this.coursesService.listAllCourses();
   }
 }
--- classroom/src/http/graphql/resolvers/enrollments.resolver.ts
+++ classroom/src/http/graphql/resolvers/enrollments.resolver.ts
@@ -1,5 +1,16 @@
+import { EnrollmentsService } from './../../../services/enrollments.service';
 import { Enrollment } from './../models/enrollment';
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from '../../../../../purchases/src/http/auth/authorization.guard';
 
 @Resolver(() => Enrollment)
-export class EnrollmentsResolver {}
+export class EnrollmentsResolver {
+  constructor(private enrollmentsService: EnrollmentsService) {}
+
+  @Query(() => [Enrollment])
+  @UseGuards(AuthorizationGuard)
+  enrollments() {
+    return this.enrollmentsService.listAllEnrollments();
+  }
+}
