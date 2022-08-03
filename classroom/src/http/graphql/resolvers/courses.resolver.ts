966794a5c5e9fe4fe1e25d74a461d363a5d4f2f7
iwatani-kenji <kleversonkenji@gmail.com>
2022-08-02 23:06:09 -0300

    finishing all queries and mutations

--- classroom/src/http/graphql/resolvers/courses.resolver.ts
+++ classroom/src/http/graphql/resolvers/courses.resolver.ts
@@ -2,17 +2,45 @@ import { CreateCourseInput } from './../inputs/create-course-input';
 import { CoursesService } from './../../../services/courses.service';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Course } from '../models/course';
-import { UseGuards } from '@nestjs/common';
+import { UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AuthorizationGuard } from '../../../../../purchases/src/http/auth/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { StudentsService } from 'src/services/students.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
 
 @Resolver(() => Course)
 export class CoursesResolver {
-  constructor(private coursesService: CoursesService) {}
+  constructor(
+    private coursesService: CoursesService,
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
 
   @Query(() => [Course])
   @UseGuards(AuthorizationGuard)
   courses() {
     return this.coursesService.listAllCourses();
+  }
+
+  @Query(() => Course)
+  @UseGuards(AuthorizationGuard)
+  async course(@Args('id') id: string, @CurrentUser() user: AuthUser) {
+    const student = await this.studentsService.getStudentByAuthUserId(user.sub);
+
+    if (!student) {
+      throw new Error('Student not found.');
+    }
+
+    const enrollment = await this.enrollmentsService.getByCourseAndStudentId({
+      courseId: id,
+      studentId: student.id,
+    });
+
+    if (!enrollment) {
+      throw new UnauthorizedException();
+    }
+
+    return this.coursesService.getCourseById(id);
   }
 
   @Mutation(() => Course)
--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -4,6 +4,7 @@ import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { StudentsService } from 'src/services/students.service';
 import { EnrollmentsService } from 'src/services/enrollments.service';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
 
 @Resolver(() => Student)
 export class StudentsResolver {
@@ -11,6 +12,12 @@ export class StudentsResolver {
     private studentsService: StudentsService,
     private enrollmentsService: EnrollmentsService,
   ) {}
+
+  @Query(() => Student)
+  @UseGuards(AuthorizationGuard)
+  me(@CurrentUser() user: AuthUser) {
+    return this.studentsService.getStudentByAuthUserId(user.sub);
+  }
 
   @Query(() => [Student])
   @UseGuards(AuthorizationGuard)
