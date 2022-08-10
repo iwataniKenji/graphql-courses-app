36495e6c83eb94687abcdd1f3bf1c5801992d474
iwatani-kenji <kleversonkenji@gmail.com>
2022-08-10 00:02:50 -0300

    stablishing communication between backend applications

--- classroom/src/messaging/messaging.module.ts
+++ classroom/src/messaging/messaging.module.ts
@@ -1,7 +1,13 @@
+import { CoursesService } from './../services/courses.service';
+import { StudentsService } from './../services/students.service';
 import { Module } from '@nestjs/common';
 import { PurchaseController } from './controllers/purchases.controller';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PurchaseController],
+  providers: [StudentsService, CoursesService, EnrollmentsService],
 })
 export class MessagingModule {}
